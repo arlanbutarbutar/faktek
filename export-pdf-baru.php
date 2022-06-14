081f9b2f14cf98b7abd1bb2d890d5d1c547b3526
arlanbutarbutar <arlan270899@gmail.com>
2022-06-14 18:33:01 +0800

    menambahkan tanda tangan pada export pdf

--- export-pdf-baru.php
+++ export-pdf-baru.php
@@ -21,26 +21,42 @@ $mpdf->WriteHTML('<table class="table table-striped" style="text-align: center;"
   </thead>
 <tbody id="search-page">
 ');
-if(mysqli_num_rows($newMahasiswa)==0){
-$mpdf->WriteHTML('<tr>
+if (mysqli_num_rows($newMahasiswa) == 0) {
+  $mpdf->WriteHTML('<tr>
 <th colspan="11">Belum ada data.</th>
 </tr>
 ');
-}$no=1; if(mysqli_num_rows($newMahasiswa)>0){while($row=mysqli_fetch_assoc($newMahasiswa)){
-  $tgl_masuk=date_create($row["tgl_masuk"]); $tgl_masuk=date_format($tgl_masuk, "d M Y");
-$mpdf->WriteHTML('<tr>
-<th scope="row">'.$no.'</th>
-<td>'.$row["noreg"].'</td>
-<td>'.$row["nama"].'</td>
-<td>'.$row["prodi"].'</td>
-<td>'.$row["jk"].'</td>
-<td>'.$row["ttl"].'</td>
-<td>'.$tgl_masuk.'</td>
-<td>'.$row["asal_sklh"].'</td>
-<td>'.$row["nilai_tes"].'</td>
+}
+$no = 1;
+if (mysqli_num_rows($newMahasiswa) > 0) {
+  while ($row = mysqli_fetch_assoc($newMahasiswa)) {
+    $tgl_masuk = date_create($row["tgl_masuk"]);
+    $tgl_masuk = date_format($tgl_masuk, "d M Y");
+    $mpdf->WriteHTML('
+<tr>
+  <th scope="row">' . $no . '</th>
+  <td>' . $row["noreg"] . '</td>
+  <td>' . $row["nama"] . '</td>
+  <td>' . $row["prodi"] . '</td>
+  <td>' . $row["jk"] . '</td>
+  <td>' . $row["ttl"] . '</td>
+  <td>' . $tgl_masuk . '</td>
+  <td>' . $row["asal_sklh"] . '</td>
+  <td>' . $row["nilai_tes"] . '</td>
 </tr>');
-$no++; }}
-$mpdf->WriteHTML('</tbody>
-</table>');
+    $no++;
+  }
+}
+$mpdf->WriteHTML('
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+</tbody>
+</table>
+<img src="assets/img/signature.png">');
 $mpdf->Output();
-
--- export-pdf-wisuda.php
+++ export-pdf-wisuda.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf();
 $mpdf->WriteHTML('<img src="assets/img/cop.png">');
 $mpdf->WriteHTML('<h2 style="text-align: center;">Data Mahasiswa Wisuda</h2>');
-$mpdf->WriteHTML('<table class="table table-striped" style="text-align: center;">
+$mpdf->WriteHTML('<table class="table table-striped" row="1" style="text-align: center;">
   <thead>
     <tr>
       <th scope="col">No</th>
@@ -25,32 +25,51 @@ $mpdf->WriteHTML('<table class="table table-striped" style="text-align: center;"
   </thead>
 <tbody id="search-page">
 ');
-if(mysqli_num_rows($wisudaMahasiswa)==0){
-$mpdf->WriteHTML('<tr>
+if (mysqli_num_rows($wisudaMahasiswa) == 0) {
+  $mpdf->WriteHTML('<tr>
 <th colspan="15">Belum ada data.</th>
 </tr>
 ');
-}$no=1; if(mysqli_num_rows($wisudaMahasiswa)>0){while($row=mysqli_fetch_assoc($wisudaMahasiswa)){
-  $tgl_masuk=date_create($row['tgl_masuk']); $tgl_masuk=date_format($tgl_masuk, "d M Y");
-  $tgl_lulus=date_create($row['tgl_lulus']); $tgl_lulus=date_format($tgl_lulus, "d M Y");
-  $tahun_wisuda=date_create($row['tahun_wisuda']); $tahun_wisuda=date_format($tahun_wisuda, "Y");
-$mpdf->WriteHTML('<tr>
-<th scope="row">'.$no.'</th>
-<td>'.$row['prodi'].'</td> 
-<td>'.$row['noreg'].'</td>
-<td>'.$row['nama'].'</td>
-<td>'.$row['jk'].'</td>
-<td>'.$row['ttl'].'</td>
-<td>'.$tgl_masuk.'</td>
-<td>'.$tgl_lulus.'</td>
-<td>'.$row['ipk'].'</td>
-<td>'.$row['predikat_lulus'].'</td>
-<td>'.$tahun_wisuda.'</td>
-<td>'.$row['wisuda_ke'].'</td>
-<td>'.$row['lama_studi'].'</td>
-</tr>');
-$no++; }}
-$mpdf->WriteHTML('</tbody>
-</table>');
+}
+$no = 1;
+if (mysqli_num_rows($wisudaMahasiswa) > 0) {
+  while ($row = mysqli_fetch_assoc($wisudaMahasiswa)) {
+    $tgl_masuk = date_create($row['tgl_masuk']);
+    $tgl_masuk = date_format($tgl_masuk, "d M Y");
+    $tgl_lulus = date_create($row['tgl_lulus']);
+    $tgl_lulus = date_format($tgl_lulus, "d M Y");
+    $tahun_wisuda = date_create($row['tahun_wisuda']);
+    $tahun_wisuda = date_format($tahun_wisuda, "Y");
+    $mpdf->WriteHTML('<tr>
+<th scope="row">' . $no . '</th>
+<td>' . $row['prodi'] . '</td> 
+<td>' . $row['noreg'] . '</td>
+<td>' . $row['nama'] . '</td>
+<td>' . $row['jk'] . '</td>
+<td>' . $row['ttl'] . '</td>
+<td>' . $tgl_masuk . '</td>
+<td>' . $tgl_lulus . '</td>
+<td>' . $row['ipk'] . '</td>
+<td>' . $row['predikat_lulus'] . '</td>
+<td>' . $tahun_wisuda . '</td>
+<td>' . $row['wisuda_ke'] . '</td>
+<td>' . $row['lama_studi'] . '</td>
+</tr>
+');
+    $no++;
+  }
+}
+$mpdf->WriteHTML('
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+<tr><td colspan="9"></td></tr>
+</tbody>
+</table>
+<img src="assets/img/signature.png">
+');
 $mpdf->Output();
-
